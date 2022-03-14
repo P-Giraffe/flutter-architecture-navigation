0984093c3ecbd76572730192b1c751ec1ec5f7b1
Maxime Britto <maxime@purplegiraffe.fr>
2022-03-14 15:44:23 +0100

    12_3_Creer_un_Widget_et_son_ViewModel

---
+++ flutter_avance/lib/ui/screens/login_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_avance/ui/screens/login_viewmodel.dart';
+
+class LoginScreen extends StatelessWidget {
+  final LoginViewModel _viewModel;
+  const LoginScreen(this._viewModel, {Key? key}) : super(key: key);
+
+  _loggedInButtonPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              autofillHints: const [
+                AutofillHints.username,
+                AutofillHints.email
+              ],
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextFormField(
+              obscureText: true,
+              autofillHints: const [AutofillHints.password],
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            Center(
+              child: ElevatedButton(
+                  onPressed: _loggedInButtonPressed,
+                  child: const Text("Sign in")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
