f5ad4633e3665a73eccf7176b1bb24533f3a9b98
Maxime Britto <maxime@purplegiraffe.fr>
2022-03-14 16:18:44 +0100

    12_6_Surveiller_un_view_model_depuis_notre_widget

--- flutter_avance/lib/ui/screens/login_screen.dart
+++ flutter_avance/lib/ui/screens/login_screen.dart
@@ -5,42 +5,50 @@ class LoginScreen extends StatelessWidget {
   final LoginViewModel _viewModel;
   const LoginScreen(this._viewModel, {Key? key}) : super(key: key);
 
-  _loggedInButtonPressed() {}
+  _loggedInButtonPressed() {
+    _viewModel.loginUser();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final errorMessage = _viewModel.errorMessage;
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              autofillHints: const [
-                AutofillHints.username,
-                AutofillHints.email
-              ],
-              decoration: const InputDecoration(labelText: "Email"),
-            ),
-            TextFormField(
-              obscureText: true,
-              autofillHints: const [AutofillHints.password],
-              decoration: const InputDecoration(labelText: "Password"),
-            ),
-            const SizedBox(height: 8),
-            if (_viewModel.isLoading)
-              const Center(child: CircularProgressIndicator())
-            else
-              Center(
-                child: ElevatedButton(
-                    onPressed: _loggedInButtonPressed,
-                    child: const Text("Sign in")),
-              ),
-            if (errorMessage != null) Text(errorMessage)
-          ],
-        ),
+        child: AnimatedBuilder(
+            animation: _viewModel,
+            builder: (context, child) {
+              final errorMessage = _viewModel.errorMessage;
+              return Column(
+                children: [
+                  TextFormField(
+                    onChanged: _viewModel.emailChanged,
+                    keyboardType: TextInputType.emailAddress,
+                    autofillHints: const [
+                      AutofillHints.username,
+                      AutofillHints.email
+                    ],
+                    decoration: const InputDecoration(labelText: "Email"),
+                  ),
+                  TextFormField(
+                    onChanged: _viewModel.passwordChanged,
+                    obscureText: true,
+                    autofillHints: const [AutofillHints.password],
+                    decoration: const InputDecoration(labelText: "Password"),
+                  ),
+                  const SizedBox(height: 8),
+                  if (_viewModel.isLoading)
+                    const Center(child: CircularProgressIndicator())
+                  else
+                    Center(
+                      child: ElevatedButton(
+                          onPressed: _loggedInButtonPressed,
+                          child: const Text("Sign in")),
+                    ),
+                  if (errorMessage != null) Text(errorMessage)
+                ],
+              );
+            }),
       ),
     );
   }
--- flutter_avance/lib/ui/screens/login_viewmodel.dart
+++ flutter_avance/lib/ui/screens/login_viewmodel.dart
@@ -1,7 +1,30 @@
-class LoginViewModel {
+import 'package:flutter/material.dart';
+
+class LoginViewModel extends ChangeNotifier {
   bool _isLoading = false;
   String? _errorMessage;
 
+  String? _email;
+  String? _password;
+
   bool get isLoading => _isLoading;
   String? get errorMessage => _errorMessage;
+
+  void emailChanged(String newEmail) {
+    _email = newEmail;
+  }
+
+  void passwordChanged(String newPassword) {
+    _password = newPassword;
+  }
+
+  void loginUser() {
+    final email = _email;
+    final password = _password;
+    if (email != null && password != null) {
+      _isLoading = true;
+      _errorMessage = "Login attempt with $email and $password";
+      notifyListeners();
+    }
+  }
 }
